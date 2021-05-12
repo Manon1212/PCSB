6a2c224676787319280c6f85e4b490793271ecfd
Manon Job <manon.job@ens.fr>
2021-05-12 19:15:35 +0200

    final

--- Project_Berlucchi.py
+++ Project_Berlucchi.py
@@ -12,11 +12,13 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 
-W, H = 1000, 1000 #Size of the graphic window
-center_x, center_y = W/2, H/2 
+Width, Height = 1000, 1000 #Size of the graphic window
+center_x, center_y = Width/2, Height/2 
+instruction_x, instruction_y = 10,  center_y/2
+
 
 def create_window():
-    screen = pygame.display.set_mode((W, H), pygame.DOUBLEBUF)
+    screen = pygame.display.set_mode((Width, Height), pygame.DOUBLEBUF)
     return screen
 
 
@@ -27,12 +29,13 @@ def clear_screen(screen):
 
 def display_instruction(screen, x, y):
     myfont = pygame.font.SysFont(pygame.font.get_fonts()[0], 32)
-
     line1 = myfont.render("A square patch of light will flash at irregular time intervals.", 1, pygame.Color('white'))
-    if key == pygame.K_q:
+    
+    if hand_lat == "left_key":
         line2 = myfont.render("Press the key a as soon as possible when you see it !", 1, pygame.Color('white'))
     else:
-        line2 =myfont.render("Press the key p as soon as possible when you see it !", 1, pygame.Color('white'))
+        line2 = myfont.render("Press the key p as soon as possible when you see it !", 1, pygame.Color('white'))
+    
     line3 = myfont.render("Press space bar to start.", 1, pygame.Color('white'))
     screen.blit(line1, (x, y))
     screen.blit(line2, (x, y + 60))
@@ -41,11 +44,11 @@ def display_instruction(screen, x, y):
 
 
 
-def present_stimulus(angle_choice, left_or_right):
+def present_stimulus(angle_choice, stimulus_lat):
     width, height = 35, 25 # dimensions of the rectangle in pixels
     left_x = center_x - width // 2  # x coordinate of topleft corner
     top_y = center_y - height // 2  # y coordinate of topleft corner
-    Rect = (left_x + angle_choice * left_or_right, top_y, width, height)
+    Rect = (left_x + angle_choice * stimulus_lat, top_y, width, height)
     pygame.draw.rect(screen, WHITE, Rect)
     pygame.display.flip()
 
@@ -101,13 +104,17 @@ def save_data(waiting_times, reaction_times, filename=RESULT_FILE):
 
 pygame.init()
 screen = create_window()
-key = random.choice([pygame.K_q, pygame.K_p])
 
+
+hand_lat = random.choice(["left_key", "right_key"]) #hand motor lateralization
+stimulus_lat = random.choice([1, -1]) #visual stimulus lateralizatioon
 
 waiting_times = []
 reaction_times = []
 
-display_instruction(screen, 10, center_y/2)
+
+
+display_instruction(screen, instruction_x, instruction_y)
 wait_for_keypress()
 clear_screen(screen)
 
@@ -115,8 +122,7 @@ pygame.time.delay(2000)
 
 
 trial_number= 1 
-left_or_right = random.choice([1, -1]) #stimulus lateralizatioon
-angle = [5, 200, 400, 5, 200, 400, 5, 200, 400, 5, 200, 400, 5, 200, 400] #distance from the center
+angle = [5, 200, 400, 5, 200, 400, 5, 200, 400, 5, 200, 400, 5, 200, 400] 
 random.shuffle(angle)
 for angle_choice in angle:
     clear_screen(screen)
@@ -124,7 +130,7 @@ for angle_choice in angle:
     waiting_time = random.randint(MIN_WAIT_TIME, MAX_WAIT_TIME)
     pygame.time.delay(waiting_time)
 
-    present_stimulus(angle_choice, left_or_right)
+    present_stimulus(angle_choice, stimulus_lat)
 
     reaction_time = measure_reaction_time()
     if reaction_time is None: # escape pressed
